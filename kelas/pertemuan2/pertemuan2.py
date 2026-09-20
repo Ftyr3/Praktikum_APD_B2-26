4330511c3786f7b53ce095ada6d3a21ebdc4b203
Daniel Sagala <danilvival16@gmail.com>
2026-09-20 21:22:31 +0800

    pertemuan 2

---
+++ kelas/pertemuan2/pertemuan2.py
@@ -0,0 +1,83 @@
+nama = "rendy"
+
+print(nama)
+
+nama_panjang = "Ahmad Rafi"
+
+print(nama_panjang)
+
+a = 10
+b = 5
+c = a + b
+
+print(c)
+
+float1 = 1.53
+float2 = 7.2
+hasil = float1 + float2
+
+print(hasil)
+
+print('''halo saya sgma
+dan saya''')
+
+nama = "Rafi "
+hobi = "Mancing"
+
+print(nama + hobi)
+
+teks = "Rahmat Flowchart"
+
+#menampilkan index ke-3
+print(teks[3])
+#menampilkan index ke-3 hingga ke-6
+print(teks[3:6])
+#menampilkan seluruhnya, tetapi dimulai dari index ke-7
+print(teks[7:])
+#menampilkan hanya sampai index ke-6
+print(teks[:6])
+
+Hujan = False
+Panas = True
+
+print(Hujan)
+
+data = ["APD", 10, True]
+print(data[0])
+print(data[1:3])
+
+data_mahasiswa = ("Rafi","034",True, 3.5)
+print(data_mahasiswa[0])
+print(data_mahasiswa[2])
+
+angka = {1, 2, 2, 2, 2, 3, 4, 4, 5}
+print(angka)
+
+mahasiswa = {
+'nim' : '91827981237',
+'nama' : 'haikal',
+'ipk' : 3.78
+}
+...
+# Mengakses Value dalam Dictionary
+print(mahasiswa['nim'])
+print(mahasiswa['nama'])
+print(mahasiswa['ipk'])
+
+angka2 = 10
+print(type(angka2))
+angka2 = str(angka2)
+print(type(angka2))
+
+#nama = input("Masukkan Nama: ")
+#print(nama)
+
+a = 5
+a += 1
+print(a)
+
+a = True
+b = False
+c = a or b
+
+print(c)
